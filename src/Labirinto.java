ad5924af72dd38253797e1ac00ee7be2aeb20226
Lucca Maia <luccamaiarosa@gmail.com>
2024-05-15 18:06:35 -0300

    Finalização do método criaLabirinto

--- src/Labirinto.java
+++ src/Labirinto.java
@@ -2,41 +2,32 @@ import java.io.*;
 
 public class Labirinto {
     private char[][] labirinto;
-    
+
     public char[][] criaLabirinto() {
         String filename = "F:\\Usuários\\delta\\Área de Trabalho\\Lucca\\Faculdade ADS\\3°Semestre\\Laboratório II\\Labirinto.txt";
         try {
             FileReader fr = new FileReader(filename);
             BufferedReader in = new BufferedReader(fr);
             String line = in.readLine();
-            int linhas = 0;
-            while (line != null) {
-                char[] linhaArray = line.toCharArray();
-                char[] linhaFormatada = new char[linhaArray.length];
-                for (int i = 0; i < linhaArray.length; i++) {
-                    if (linhaArray[i] == 'X') {
-                        linhaFormatada[i] = ' ';
-                    } else if (linhaArray[i] == ' ') {
-                        linhaFormatada[i] = (i == 0 || linhaArray[i - 1] != ' ') ? ' ' : '\0';
-                    } else {
-                        linhaFormatada[i] = linhaArray[i];
-                    }
-                }
-                if (labirinto == null) {
-                    labirinto = new char[1][linhaArray.length];
-                } else {
-                    char[][] novoLabirinto = new char[linhas + 1][linhaArray.length];
-                    for (int i = 0; i < linhas; i++) {
-                        novoLabirinto[i] = labirinto[i];
-                    }
-                    labirinto = novoLabirinto;
-                }
-                labirinto[linhas] = linhaFormatada;
+            String[] colunas = line.split("\\,");
+            int numColunas = colunas.length;
+            int numLinhas = 0;
+            while(line != null) {
+                numLinhas++;
                 line = in.readLine();
-                linhas++;
             }
             in.close();
-            return labirinto;
+            FileReader fr2 = new FileReader(filename);
+            BufferedReader in2 = new BufferedReader(fr2);
+            labirinto = new char[numLinhas][numColunas];
+            for (int i = 0; i < numLinhas; i++) {
+                line = in2.readLine();
+                line = line.replaceAll(",",""); 
+                for (int j = 0; j < numColunas; j++) {
+                    labirinto[i][j] = line.charAt(j);
+                }
+            }
+            in2.close();
         }
         catch (FileNotFoundException e) {
             System.out.println("Arquivo \""+filename+" \" não existe.");
@@ -44,7 +35,7 @@ public class Labirinto {
         catch (IOException e) {
             System.out.println("Erro na leitura de " + filename+ ".");
         }
-        return null;
+        return labirinto;
     }
     public percorreLabirinto() {
 
